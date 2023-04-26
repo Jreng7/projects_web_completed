aeac5facdc0babe466c6a5f7375ae92661f6bb95
Josue R S Jacinto <jr.waydev@gmail.com>
2023-04-25 23:58:31 -0300

    alteracao style.css

--- project_06/main.js
+++ project_06/main.js
@@ -1,4 +1,13 @@
 
+// Index         0       1        2        3
 let friends = ['Alan', 'Mark', 'Julia', 'Bella']
+console.log(friends[1]) // Pegue o index na posição 1, logo, retornará "Mark"
 
-console.log(friends)
+
+friends[1] = 'John' // O nome dessa mudança entre "Mark" para "John" é REPLACE.
+
+friends[4] = 'Doe' /* Como não existe index 4, aqui não será replace, mas ADD ITEM
+Ou seja, adicionando item ao Array cujo nome é "Friends. Lembrando que
+Array em português significa lista. */
+
+console.log(friends[1])

4667cbbe06de3af14b2315a3e31f874e3d58dd97
Josue R S Jacinto <jr.waydev@gmail.com>
2023-04-28 23:46:20 -0300

    more change

--- project_08/main.js
+++ project_08/main.js
@@ -4,6 +4,8 @@ let nota = 7.7
 
 if(nota >= 7) {
     console.log(`Parabéns, você foi aprovado, sua nota foi: ${nota}`)
-} else {
+} else if (nota <= 5.9) {
     console.log('Infelizmente você foi reprovado.')
+} else {
+    console.log('Você está de recuperação.')
 }

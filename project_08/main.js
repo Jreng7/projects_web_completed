cd8eddba46fb78ae7bf321da7c3009a3040f47c6
Josue R S Jacinto <jr.waydev@gmail.com>
2023-04-28 23:56:20 -0300

    project 08 - mudanças

--- project_08/main.js
+++ project_08/main.js
@@ -3,6 +3,24 @@ let grade = prompt('Digite a nota do aluno: ')
 
 if (grade > 100) {
     alert('Nota inválida.')
-} else if (grade >= 90 ) {
+} 
+
+else if (grade >= 90 ) {
     console.log('A nota é: A')
+}
+
+else if (grade >= 80 ) {
+    console.log('A nota é: B')
+}
+
+else if (grade >= 70 ) {
+    console.log('A nota é: C')
+}
+
+else if (grade >= 60 ) {
+    console.log('A nota é: D')
+}
+
+else {
+    console.log('Aluno reprovado.')
 }

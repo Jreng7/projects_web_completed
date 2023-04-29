e9544529e6d6bf0ad50cb4400f8cabc83f1a59d5
Josue R S Jacinto <jr.waydev@gmail.com>
2023-04-28 23:35:23 -0300

    finalizando proj 7

--- project_07/main.js
+++ project_07/main.js
@@ -1,6 +1,9 @@
 
-let celcius = prompt('Digite a temperatura em Celcius: ')
+let celcius = 0;
+let fahrenheit = 0;
+
+celcius = parseFloat(prompt('Digite a temperatura em Celcius: '))
 
 fahrenheit = celcius * 1.8 + 32
 
-alert(`A temperatura ${celcius} em Fahrenheit é ${fahrenheit}`)
+alert(`A temperatura ${celcius} em Fahrenheit é ${fahrenheit.toFixed(2)}`)
